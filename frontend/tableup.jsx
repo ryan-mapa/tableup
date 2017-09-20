04e2220391b8c5c6f7d52682c508d98bfabc964b
ryan mapa <ryan.m.mapa@gmail.com>
2017-09-20 14:47:08 -0700

    Add root reducer

--- frontend/tableup.jsx
+++ frontend/tableup.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {login, signup, logout} from './util/session_api_util.js';
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
+
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Yo sup TableUP</h1>, root);
 });
