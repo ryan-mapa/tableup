ddf5c99aa5b46d7cff6d95edb72a8c86552a5a32
ryan mapa <ryan.m.mapa@gmail.com>
2017-09-23 23:26:07 -0700

    Create restaurant search index.json.jbuilder

--- app/controllers/restaurant_searches_controller.rb
+++ app/controllers/restaurant_searches_controller.rb
@@ -2,6 +2,7 @@ class RestaurantSearchesController < ApplicationController
 
   def index
     @restaurants = Restaurant.search_results(search_params[:query])
+    render :index
   end
 
   def search_params
