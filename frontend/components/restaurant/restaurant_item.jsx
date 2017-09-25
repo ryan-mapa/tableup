dd8a91ffe935b68a650c1f30a2bb8f3b09d9f998
ryan mapa <ryan.m.mapa@gmail.com>
2017-09-24 20:37:20 -0700

    Add images and styling to restaurant index items

--- frontend/components/restaurant/restaurant_item.jsx
+++ frontend/components/restaurant/restaurant_item.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 const RestaurantItem = ({restaurant}) => (
-  <div>
-    {restaurant.name}
-    <br /><span>City: </span>{restaurant.city}
-    <br /><span>Cuisine: </span>{restaurant.cuisine}
-    <br /><span>Rating: </span>{restaurant.rating}
-    <br /><span>Avg. Price: $</span>{restaurant.price}
-    <br /><span>Description: </span>{restaurant.description}
-    <br /><span>profile_image url: </span>{restaurant.profile_image}
+  <div className='restaurant-index-item'>
+    <span>
+      <img src={restaurant.profile_image}></img>
+    </span>
+    <span className='restaurant-info'>
+      <h2>{restaurant.name}</h2>
+      <br /><span>City: </span>{restaurant.city}
+      <br /><span>Cuisine: </span>{restaurant.cuisine}
+      <br /><span>Rating: </span>{restaurant.rating}
+      <br /><span>Avg. Price: $</span>{restaurant.price}
+      <br /><span>Description: </span>{restaurant.description}
+      <br /><span>profile_image url: </span>{restaurant.profile_image}
+    </span>
     <br />
     <br />
   </div>
