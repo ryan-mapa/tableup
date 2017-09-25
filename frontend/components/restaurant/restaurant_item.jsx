9af59b388bd4880cb3cf7f8084cadc3702329ff4
ryan mapa <ryan.m.mapa@gmail.com>
2017-09-24 20:57:32 -0700

    Update restaurant item styling

--- frontend/components/restaurant/restaurant_item.jsx
+++ frontend/components/restaurant/restaurant_item.jsx
@@ -3,16 +3,15 @@ import React from 'react';
 const RestaurantItem = ({restaurant}) => (
   <div className='restaurant-index-item'>
     <span>
-      <img src={restaurant.profile_image}></img>
+      <img className='profile-image' src={restaurant.profile_image}></img>
     </span>
     <span className='restaurant-info'>
       <h2>{restaurant.name}</h2>
       <br /><span>City: </span>{restaurant.city}
       <br /><span>Cuisine: </span>{restaurant.cuisine}
       <br /><span>Rating: </span>{restaurant.rating}
-      <br /><span>Avg. Price: $</span>{restaurant.price}
-      <br /><span>Description: </span>{restaurant.description}
-      <br /><span>profile_image url: </span>{restaurant.profile_image}
+      <br /><span>Avg. Price: $</span>{restaurant.price}<br />
+      <br /><h3>Description: </h3>{restaurant.description}
     </span>
     <br />
     <br />
