0ac30d999b2467c42cbeb19b256a8d0bca12cf41
ryan mapa <ryan.m.mapa@gmail.com>
2017-09-24 22:59:30 -0700

    Add star ratings

--- frontend/components/restaurant/restaurant_item.jsx
+++ frontend/components/restaurant/restaurant_item.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 
-const RestaurantItem = ({restaurant}) => (
-  <div className='restaurant-index-item'>
-    <span>
-      <img className='profile-image' src={restaurant.profile_image}></img>
-    </span>
-    <span className='restaurant-info'>
-      <h2>{restaurant.name}</h2>
-      <br /><span>City: </span>{restaurant.city}
-      <br /><span>Cuisine: </span>{restaurant.cuisine}
-      <br /><span>Rating: </span>{restaurant.rating}
-      <br /><span>Avg. Price: $</span>{restaurant.price}<br />
-      <br /><h3>Description: </h3>{restaurant.description}
-    </span>
-    <br />
-    <br />
-  </div>
-);
+const RestaurantItem = ({restaurant}) => {
+  let length = Math.round(restaurant.rating);
+  let stars = "";
+  for (var i = 0; i < length; i++) {
+    stars += "★";
+  }
+  return (
+    <div className='restaurant-index-item'>
+      <span>
+        <img className='profile-image' src={restaurant.profile_image}></img>
+      </span>
+      <span className='restaurant-info'>
+        <h2>{restaurant.name}</h2>
+
+        <br /><span>City: </span>{restaurant.city}
+        <br /><span>Cuisine: </span>{restaurant.cuisine}
+        <br /><span>Rating: </span><span className='star-rating'>{stars}</span>
+        <br /><span>Avg. Price: $</span>{restaurant.price}<br />
+        <br /><h3>Description: </h3>{restaurant.description}
+      </span>
+      <br />
+      <br />
+    </div>
+  );
+};
 
 export default RestaurantItem;
