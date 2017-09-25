7fcdb0f3a7c6a52fa061725a3cb938a5fbeba9fc
ryan mapa <ryan.m.mapa@gmail.com>
2017-09-25 09:30:59 -0700

    create reservations controller

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,13 @@
+class ReservationsController < ApplicationController
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'reservations/create'
+
+  get 'reservations/show'
+
+  get 'reservations/index'
+
+  get 'reservations/destroy'
+
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
