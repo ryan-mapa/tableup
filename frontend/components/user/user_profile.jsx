ca0049fc6936f59b0f7f6279b7bd3cb703e88408
ryan mapa <ryan.m.mapa@gmail.com>
2017-09-27 11:39:42 -0700

    Create user components for profile page

---
+++ frontend/components/user/user_profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class UserProfile extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Yo user profile
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
